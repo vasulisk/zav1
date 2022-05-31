b3278d64cafde3a01c12c258ddb2fa4749c72266
vasulisk <99254109+vasulisk@users.noreply.github.com>
2022-05-31 09:50:51 +0200

    Add files via upload

---
+++ zav5/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 2;
+        for(a = 200; a < 1000; a = 2*a - 1 ){
+            System.out.println(a);
+        }
+    }
+}
